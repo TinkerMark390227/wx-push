cf9eab868a20b336d27b6541456896fa889a9134
TinkerMark390227 <1197509212@qq.com>
2023-08-07 09:46:14 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,10 +1,10 @@
 module.exports = {
-  appid: "wx71941f6106ae1072",
-  appsecret: "b4b2ad8a8d4a94e403831886ac10c646",
-  template_id: "q14lTqsDyK0N-KcZUs8UKHOl3GWn2uErDk-8hbm2ouQ",
-  touser: "oonWJ5slWuS2OrJdgCLM3GmNdxVk",
+  appid: "wxf7de7f31f56086e3",
+  appsecret: "72c81c979a2d15264b3b29c8ad7500d9",
+  template_id: "vpt5NfBuHmY_PkoSJTRpjACnA2qgE4T8Cy7KUkek5GY",
+  touser: "oixnN6aVZEIZNtq7uKFVaJOCAIiM",
 
   key: "201c93c2e2b34bdeecdbf9f5265eb84e",
-  city: "邢台",
+  city: "合肥",
   first_date: "2022-11-10",
 };
